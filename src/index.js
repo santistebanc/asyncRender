aa236ed1405254a0b5093617b6e4f6433b4120c7
santistebanc <santistebanc94@gmail.com>
2022-03-05 17:13:38 +0100

    restructure

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-// import { Computed, State } from './State'
-// import Tree from './Tree'
-// import { run } from './utils'
+import Component from './Component'
+import { Context } from './Context'
 
-// const timer = State(0)
-// const page = State('main')
+const color = Context('red')
 
-// // const app = Tree({
-// //   all: () =>
-// //     page.value === 'main'
-// //       ? {
-// //           title: () => page + '-' + timer,
-// //           other: { go: 1 },
-// //           error: '-',
-// //         }
-// //       : { title: 'not main', error: 'ERR' },
-// // })
+const app = Component({
+  [color]: 'blue',
+  zero: { one: { two: () => color() } },
+})
 
-// const app = Tree({ title: 'not main', error: 'ERR' })
-
-// // run(app.all.title, v => console.log(v))
-// run(app, v => console.log(v.value))
-
-// setTimeout(() => {
-//   page.value = 'not'
-// }, 2000)
-
-import './concept'
+console.log(app.zero.one.two())
